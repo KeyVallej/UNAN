107090c546840c02eb3f8b5dee19b51113ad09c8
Felix Lopez <lfelixfermin@gmail.com>
2023-05-21 17:05:31 -0600

    ADD:Boton prueba

--- UNAN/Form1.Designer.cs
+++ UNAN/Form1.Designer.cs
@@ -29,13 +29,24 @@ namespace UNAN
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(467, 182);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // frmMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(970, 540);
+            this.Controls.Add(this.button1);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmMenu";
@@ -47,6 +58,8 @@ namespace UNAN
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
